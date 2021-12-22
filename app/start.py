200f699e8fc25d01a8a24eeb8be3bd17adecc23c
ayub <ayub@deepgreyresearch.com>
2021-12-22 15:58:42 +0530

    app

--- app/start.py
+++ app/start.py
@@ -1,3 +1,15 @@
-print("Hello")
+import os
+import time
+import kafka_helper
 
 
+
+pro = kafka_helper.get_kafka_producer()
+pro.send('Market-Data', key='my key', value={'k': 'v'})
+
+
+consumer = kafka_helper.get_kafka_consumer(topic='Market-Data')
+for message in consumer:
+    print(message)
+
+time.sleep(10)
